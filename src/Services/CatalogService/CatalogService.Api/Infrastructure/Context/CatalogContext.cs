b358af53e9ec68a9e8ef6d3d63323fbfdc134d57
Mücahit Tanacıoğlu <mucahittanacioglu@gmail.com>
2022-09-11 03:43:47 +0300

    add catalog service

---
+++ src/Services/CatalogService/CatalogService.Api/Infrastructure/Context/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using CatalogService.Api.Core.Domain.Entities;
+using CatalogService.Api.Infrastructure.EntityConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatalogService.Api.Infrastructure.Context
+{
+    public class CatalogContext:DbContext
+    {
+        public const string DEFAULT_SCHEMA = "catalog";
+
+        public CatalogContext(DbContextOptions<CatalogContext> options): base(options) { }
+        
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CatalogBrandEntityTypeConfiguration());
+            builder.ApplyConfiguration(new CatalogItemEntityConfiguration());
+            builder.ApplyConfiguration(new CatalogTypeEntityTypeConfugiration());
+
+        }
+    }
+}
---
+++ src/Services/CatalogService/CatalogService.Api/Infrastructure/EntityConfiguration/CatalogItemEntityConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using CatalogService.Api.Core.Domain.Entities;
+using CatalogService.Api.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CatalogService.Api.Infrastructure.EntityConfiguration
+{
+    public class CatalogItemEntityConfiguration : IEntityTypeConfiguration<CatalogItem>
+    {
+        public void Configure(EntityTypeBuilder<CatalogItem> builder)
+        {
+            builder.ToTable("Catalog", CatalogContext.DEFAULT_SCHEMA);
+
+            builder.Property(ci => ci.Id)
+                .UseHiLo("catalog_item_hilo")
+                .IsRequired();
+
+            builder.Property(ci => ci.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(ci => ci.Price)
+                .IsRequired();
+
+            builder.Property(ci => ci.PictureFileName)
+                .IsRequired(false);
+
+            builder.Ignore(ci => ci.PictureUri);
+
+            builder.HasOne(ci => ci.CatalogBrand)
+                .WithMany()
+                .HasForeignKey(ci => ci.CatalogBrandId);
+
+            builder.HasOne(ci => ci.CatalogType)
+                .WithMany()
+                .HasForeignKey(ci => ci.CatalogTypeId);
+        }
+    }
+}
